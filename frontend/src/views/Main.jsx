e0531c907722f1309a191d501422aa7ef70e3ed1
Kevin Pacheco <k.pacheco@me.com>
2020-08-12 12:37:25 -0700

    Added response design to Main.js

--- frontend/src/views/Main.jsx
+++ frontend/src/views/Main.jsx
@@ -5,27 +5,42 @@ const backgroundURL = "https://images.unsplash.com/photo-1513618364580-fe1596762
 
 export default function Main(props) {
     return (
-        <div style={
-            {
-                backgroundImage: `url(${backgroundURL})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: 'cover'
-            }
-        } className="bg-gray-500 h-screen flex flex-col justify-between">
-            {/* This is the header section */}
-            <div className="pl-16 sm:pl-20 pt-16 pb-8 bg-black bg-opacity-25">
-                <Icon className="h-12 w-12 text-orange-500 bg-white rounded-lg p-1 fill-current" />
-                <span className="block mt-5 text-2xl text-white leading-none font-semibold">
-                    Welcome all
-                    <br />
-                    foodies!
-                </span>
+        <div className="h-screen md:flex">
+            {/* Background image */}
+            <div
+                style={
+                    {
+                        backgroundImage: `url(${backgroundURL})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: 'cover',
+                        zIndex: -1
+                    }
+                }
+                className="absolute md:static left-0 top-0 w-full md:w-1/2 h-full">
+                {/* 50% transparent veil */}
+                <div
+                    style={{
+                        zIndex: -1
+                    }}
+                    className="visible md:hidden bg-black bg-opacity-50 h-full">
+                </div>
             </div>
+            <div className="flex flex-col justify-between md:justify-center md:space-y-10 md:w-1/2 h-full md:bg-black md:bg-opacity-50">
+                {/* This is the header section */}
+                <div className="pt-8 mx-auto my-auto md:my-0 md:flex-none">
+                    <Icon className="h-20 w-20 text-orange-500 bg-white rounded-lg p-1 fill-current mx-auto" />
+                    <span className="block mt-2 text-2xl text-center text-white leading-none font-semibold">
+                        Welcome all
+                        <br />
+                        foodies!
+                    </span>
+                </div>
 
-            {/* This is the buttons */}
-            <div className="flex flex-col items-center space-y-3 mb-16 px-16 sm:px-20">
-                <button className="px-3 py-2 w-full rounded-full bg-orange-500 text-white">Sign up</button>
-                <button className="px-3 py-2 w-full rounded-full bg-white text-orange-500">Sign in</button>
+                {/* This is the buttons */}
+                <div className="flex flex-col w-full items-center space-y-3 px-5 pb-10 max-w-sm mx-auto md:flex-none">
+                    <button className="px-3 py-2 w-full rounded-full bg-orange-500 text-white">Sign up</button>
+                    <button className="px-3 py-2 w-full rounded-full bg-white text-orange-500">Sign in</button>
+                </div>
             </div>
         </div>
     );
